687a575d2d59d14c9d39593c9852af9177eaef6a
goodness <goodyc474@gmail.com>
2023-11-02 08:53:44 +0100

    initial commit

---
+++ constants/index.tsx
@@ -0,0 +1,28 @@
+export const NavBarLinks = [
+  {
+    name: "Home",
+    path: "/",
+    active: false,
+  },
+
+  {
+    name: "Products",
+    path: "/Products",
+    active: false,
+  },
+  {
+    name: "Pages",
+    path: "/",
+    active: false,
+  },
+  {
+    name: "Blog",
+    path: "/Products",
+    active: false,
+  },
+  {
+    name: "Contact",
+    path: "/contacts",
+    active: false,
+  },
+];
